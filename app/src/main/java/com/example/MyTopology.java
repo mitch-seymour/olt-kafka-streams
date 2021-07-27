e2b1193675a71c7993c669f590a5d848bf7c8cd2
Mitch Seymour <mseymour@rsglab.com>
2021-07-26 20:39:48 -0400

    Print the output

--- app/src/main/java/com/example/MyTopology.java
+++ app/src/main/java/com/example/MyTopology.java
@@ -1,14 +1,23 @@
 package com.example;
 
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Printed;
 
 class MyTopology {
 
   public static Topology build() {
     StreamsBuilder builder = new StreamsBuilder();
 
-    // add processors here
+    // add a source processor that reads data from the "tweets" topic
+    KStream<byte[], String> stream =
+        builder.stream("tweets", Consumed.with(Serdes.ByteArray(), Serdes.String()));
+
+    // print the output
+    stream.print(Printed.<byte[], String>toSysOut().withLabel("tweets-stream"));
 
     return builder.build();
   }
