7b6fdb39eb5b09ddb20ba01092084adb9d967c3d
Mitch Seymour <mseymour@rsglab.com>
2021-07-27 13:47:32 -0400

    Simplify topology and inputs

--- app/src/main/java/com/example/MyTopology.java
+++ app/src/main/java/com/example/MyTopology.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
 
@@ -10,8 +12,12 @@ class MyTopology {
   public static Topology build() {
     StreamsBuilder builder = new StreamsBuilder();
 
-    KStream<byte[], byte[]> stream = builder.stream("tweets");
-    stream.print(Printed.<byte[], byte[]>toSysOut().withLabel("tweets-stream"));
+    // read from the `tweets` topic
+    KStream<byte[], String> tweets =
+        builder.stream("tweets", Consumed.with(Serdes.ByteArray(), Serdes.String()));
+
+    // print the contents of each stream for debugging purposes
+    tweets.print(Printed.<byte[], String>toSysOut().withLabel("tweets"));
 
     return builder.build();
   }
